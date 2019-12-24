3b48513305265fd7e452bd20fccf2c3c0071ff3d
ehdgns3136 <ehdgns3136@gmail.com>
2019-12-24 11:20:32 +0900

    test commit 2

--- src/type.kt
+++ src/type.kt
@@ -2,3 +2,32 @@ fun unitFunction(): Unit {
     println("Hello")
     return
 }
+
+fun testPlatformType() {
+
+}
+
+fun safeCallWithLet(a: String?) {
+    a?.let(::println)
+}
+
+fun safeCallWithLetAndElvis(a: String?) {
+    a?.let {
+        println(it)
+    } ?: println("Hello1!!")
+}
+
+open class A(){
+
+}
+
+class B : A{
+    constructor () : super() {
+
+    }
+}
+
+fun testAsWithQuestionMark(o: Any) {
+//    val a: A? = o as? A ?:
+
+}
